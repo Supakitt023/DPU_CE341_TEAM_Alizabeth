f32bea0e1faf43aa9d6704a312eaf4da533b8122
Supakitt023 <blelslt.023@gmail.com>
2023-11-17 01:01:39 +0700

    Update script popup_image

--- javascript/pupup.js
+++ javascript/pupup.js
@@ -1,25 +1,25 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+//ดึงค่าหน้าต่าง
+var windows = document.getElementById("myModal");
 
-// Get the button that opens the modal
+// ดึงค่าปุ่มที่จะเปิดหน้าต่าง
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//ดึงค่าปุ่มที่จะปิดหน้าต่าง
+var spanClose = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+//เมื่อคลิกเมาส์ จะทำการเปิดหน้าต่างเพื่อแสดงรูป
 btn.onclick = function() {
-  modal.style.display = "block";
+  windows.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+//เมื่องคลิกเมาส์ที่ปุ่มนี้ จะปิดหน้าต่างลงไป
+spanClose.onclick = function() {
+  windows.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+//ถ้าคลิกเมาส์ภายนอกหน้าต่างหรือพื้นที่ที่กำหนดนี้ ก็จะปิดหน้าต่างลง
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    windows.style.display = "none";
   }
 }
